17b39770420a8885d9cd2678adb503016e26db3d
noovertime7 <cc17854330572@163.com>
2022-07-16 21:40:55 +0800

    informer

--- get_deployment/main.go
+++ get_deployment/main.go
@@ -4,12 +4,13 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"os"
+	"path/filepath"
+
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"k8s.io/client-go/tools/clientcmd"
-	"os"
-	"path/filepath"
 )
 
 func main() {
@@ -17,28 +18,27 @@ func main() {
 	var config *rest.Config
 	var kubeconfig *string
 
-	//获取kubeconfig
-	if home := homedir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "使用 --kubeconfig命令指定kubeconfig")
+	if home := homeDir(); home != "" {
+		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
-		kubeconfig = flag.String("kubeconfig", "", "")
+		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
 	}
 	flag.Parse()
 
-	// 使用 incluster 模式
+	// 使用 ServiceAccount 创建集群配置（InCluster模式）
 	if config, err = rest.InClusterConfig(); err != nil {
-		//使用 kubeconfig创建集群配置
+		// 使用 KubeConfig 文件创建集群配置
 		if config, err = clientcmd.BuildConfigFromFlags("", *kubeconfig); err != nil {
 			panic(err.Error())
 		}
 	}
 
+	// 创建 clientset
 	clientset, err := kubernetes.NewForConfig(config)
 	if err != nil {
 		panic(err.Error())
 	}
 
-	//使用clientset对象获取资源对象，进行crud
 	// 使用 clientsent 获取 Deployments
 	deployments, err := clientset.AppsV1().Deployments("default").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
@@ -47,11 +47,12 @@ func main() {
 	for idx, deploy := range deployments.Items {
 		fmt.Printf("%d -> %s\n", idx+1, deploy.Name)
 	}
+
 }
 
-func homedir() string {
+func homeDir() string {
 	if h := os.Getenv("HOME"); h != "" {
 		return h
 	}
-	return os.Getenv("USERPROFILE")
+	return os.Getenv("USERPROFILE") // windows
 }
